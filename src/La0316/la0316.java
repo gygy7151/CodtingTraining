89ca18d99c7989ff4c7899a97f2fcc8d3c509d9d
82108 <82108@DESKTOP-5AT7L5F>
2021-03-17 20:51:18 +0900

    연습문제1번 미션완료

--- src/La0316/la0316.java
+++ src/La0316/la0316.java
@@ -1,10 +1,28 @@
+/**1.인사하기 : 이름을 입력받아 이름을 이용하여 인사말을 출력하는 프로그램을 작성해보자*/
+
 package La0316;
+
+import java.util.Scanner;
 
 public class la0316 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) { 
+		
+		//입력부분
+		System.out.println("--------------");
+		System.out.println("성함을 입력해주세요:");
+		
+		//문자열연결부분
+		Scanner name = new Scanner(System.in.);
+		String nameeString;
+		nameeString = name.nextLine();
+		
+		//출력부분
+		System.out.println("Hello, "+ nameeString +" nice to meet you!");
+		
 
 	}
 
 }
+
+
